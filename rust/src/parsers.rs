a5c5de826885e0ec81107bab2d10699dd601bf2f
Brendan-Reid1991 <brendan.reid1991@gmail.com>
2025-12-01 23:42:42 +0000

    added formatting options

--- rust/src/parsers.rs
+++ rust/src/parsers.rs
@@ -3,7 +3,8 @@ use crate::reader::{read_puzzle_input, PuzzleInput, RawData};
 pub type Day1T = PuzzleInput<i32>;
 
 pub fn parse_day_1() -> Day1T {
-    let to_be_parsed: RawData = read_puzzle_input(1).expect("Could not read the day 1 file");
+    let to_be_parsed: RawData =
+        read_puzzle_input(1).expect("Could not read the day 1 file");
     to_be_parsed
         .iter()
         .map(|line: &String| {
