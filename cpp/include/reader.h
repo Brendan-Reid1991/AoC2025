1b291e9ada829cbfdda7b149ed1ac734c01f9960
Brendan-Reid1991 <brendan.reid1991@gmail.com>
2025-12-22 22:39:41 +0000

    implemented parser for day3

--- cpp/include/reader.h
+++ cpp/include/reader.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <filesystem>
 #include <fstream>
 #include <iostream>
 #include <string>
@@ -7,17 +8,26 @@
 
 using StringInput = std::vector<std::string>;
 
-inline std::string PuzzleFolder = "puzzle_input/";
+/// @brief Return the project root directory,
+/// assuming puzzle inputs are kept inside root/puzzle_input/
+/// and this function inside root/cpp/include/reader.h
+/// @return std::string
+inline std::string relative_pathway() {
+  std::filesystem::path header_path = __FILE__;
+  return header_path.parent_path().parent_path().parent_path().string() +
+         "/puzzle_input/";
+}
 
 inline std::string Pathway(uint day) {
-  return PuzzleFolder + "day" + std::to_string(day) + ".txt";
+  return relative_pathway() + "day" + std::to_string(day) + ".txt";
 }
 
 inline StringInput read_puzzle_input(uint day) {
   StringInput puzzle_input;
-  std::ifstream file(Pathway(day));
+  std::string path = Pathway(day);
+  std::ifstream file(path);
   if (!file.is_open()) {
-    std::cerr << "Failed to open file" << std::endl;
+    std::cerr << "Failed to open file: " << path << std::endl;
     return puzzle_input;
   }
   std::string line;
