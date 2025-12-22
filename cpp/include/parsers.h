0980415341312aa1f6221986b4b7b848e047d1dc
Brendan-Reid1991 <brendan.reid1991@gmail.com>
2025-12-22 22:39:24 +0000

    made pathway absolute

--- cpp/include/parsers.h
+++ cpp/include/parsers.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "reader.h"
 #include <utility>
+#include <vector>
 
 using Day1T = int;
 
@@ -11,6 +12,19 @@ inline PuzzleInput<Day1T> parse_day_1() {
     char direction = line[0];
     int value = std::stoi(line.substr(1));
     parsed.push_back((direction == 'R') ? -value : value);
+  }
+  return parsed;
+}
+
+using Day3T = std::vector<uint8_t>;
+inline PuzzleInput<Day3T> parse_day_3() {
+  StringInput to_be_parsed = read_puzzle_input(3);
+  PuzzleInput<Day3T> parsed;
+  for (const auto &line : to_be_parsed) {
+    std::vector<uint8_t> joltage;
+    for (char c : line)
+      joltage.push_back(c - '0');
+    parsed.push_back(joltage);
   }
   return parsed;
 }
